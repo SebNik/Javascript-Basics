d6350d04a8dd383503441bd6e96ee8e5d36abadf
Niklas <lord.voldemort1492@gmail.com>
2020-05-03 14:26:01 +0200

    workign function with retunr

--- 06-Functions/index.js
+++ 06-Functions/index.js
@@ -1,8 +1,16 @@
+// Prefomring a task
 function doSomth () {
     console.log('Hello');
 }
+
+// Calcluating function
 function sum(a,b) {
     console.log(a+b)
 }
-
 sum(5,5)
+// the right way to do it
+function square(a) {
+    return a*a;
+}
+let number = square(12)
+console.log(number)
