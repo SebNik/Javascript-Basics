b4d4371e2cb45acb1b64440cfca57d684ecd754a
Niklas <lord.voldemort1492@gmail.com>
2020-05-03 14:09:37 +0200

    First object cretaed and value changed

---
+++ 04-Objects/index.js
@@ -0,0 +1,15 @@
+let firstName = "Seb";
+let lastname = "Nik";
+
+// Now we are creating a object
+let person = {
+    nameFirst: firstName,
+    nameLast: lastname,
+    age: 30
+};
+
+console.log(person.age)
+// Changing Value
+person.age = 50
+// Printing out changed value
+console.log(person.age)
