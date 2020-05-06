fba98ecf28b4b7e8296094fb6e641a01c97deacd
Niklas <lord.voldemort1492@gmail.com>
2020-05-06 16:02:19 +0200

    rewritten function to arrow function

--- 07-React-App/first-app/src/components/start.js
+++ 07-React-App/first-app/src/components/start.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function Start(){
-    return <h1>Hello Niklas!</h1>
-}
+//function Start(){
+//    return <h1>Hello Niklas!</h1>
+//}
+
+const Start = () => <h1>Hello Niklas!</h1>
 
 export default Start
