b0368ca1df66fe024fb0f506338ef5438fe1381f
Niklas <lord.voldemort1492@gmail.com>
2020-05-06 16:58:45 +0200

    back to no matter what name

--- 07-React-App/first-app/src/App.js
+++ 07-React-App/first-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Start} from './components/start'
+import Start from './components/start'
 
 
 function App() {
