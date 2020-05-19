a00cef77965ff0c9b85f46039848302c6891dd60
Niklas <lord.voldemort1492@gmail.com>
2020-05-19 17:05:13 +0200

    added class componetn and now working there with click button

--- 07-React-App/first-app/src/App.js
+++ 07-React-App/first-app/src/App.js
@@ -7,12 +7,13 @@ import Print from './components/Print'
 import Sum from './components/Sum'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <FunctionClick />
+      <ClassClick />
     </div>
     );
   }
