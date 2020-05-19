fede636725e84235a4264e18ec11a5a478406010
Niklas <lord.voldemort1492@gmail.com>
2020-05-19 16:34:12 +0200

    activated Counter in App.js

--- 07-React-App/first-app/src/App.js
+++ 07-React-App/first-app/src/App.js
@@ -5,12 +5,13 @@ import Start from './components/start'
 import Moin from './components/Moin'
 import Print from './components/Print'
 import Sum from './components/Sum'
+import Counter from './components/Counter'
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <Sum a="5" b="8" lul="lalala" />
+      <Counter />
     </div>
     );
   }
